e762f6469886ac2be67cd2f55ff4340a2b14e6e3
gongyoon93 <qkszn93@gmail.com>
2022-05-19 21:54:06 +0900

    first api & landing page

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,10 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+export const API_KEY = '5abf6ad647702f6b442c49973a209b37';
+
+export const MOVIE_LANGUAGE = 'ko-KR';
--- client/src/components/views/LandingPage/Landingpage.js
+++ client/src/components/views/LandingPage/Landingpage.js
@@ -1,36 +1,55 @@
 import React, { useEffect } from 'react'
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { API_URL, API_KEY, MOVIE_LANGUAGE } from '../../Config';
+import MainImage from './section/MainImage';
+import { IMAGE_BASE_URL } from '../../Config';
 
 function Landingpage() {
-  const navigate = useNavigate();
-    // useEffect(() => {
-    //     axios.get('/api/hello')
-    //     .then(res => console.log(res.data));
-    // },[]);
+ 
+  const [Movies, setMovies] = useState([]);
+  const [MainMovieImage, setMainMovieImage] = useState(null);
 
-  const onClickHandler = () => {
-      axios.get('api/users/logout')
-      .then(res => {
-        if(res.data.success){
-          alert('로그아웃 되었습니다.');
-          navigate('/login');
-        }else{
-          alert('로그아웃에 실패하였습니다.');
-        }
-      });
-  };
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=${MOVIE_LANGUAGE}&page=1`;
+
+    fetch(endpoint)
+    .then(res => res.json())
+    .then(res => {
+       setMovies([res.results]);
+       setMainMovieImage(res.results[0]);
+    });
+
+  }, []);
 
   return (
-    <div style={{
-      display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
-      , width: '100%', height: '100vh'
-    }}>
-      <h2>시작 페이지</h2>
+    <div style={{ width: '100%', margin: '0' }}>
   
-      <button onClick={onClickHandler}>
-        로그아웃</button>
+      {/* Main Image */}
+
+      {
+        MainMovieImage && <MainImage 
+        image={`${IMAGE_BASE_URL}original${MainMovieImage.backdrop_path}`}
+        title={MainMovieImage.original_title}
+        text={MainMovieImage.overview}
+        />
+      }
+
+      <div style={{ width: '85%', margin: '1rem auto'}}>
+
+        <h2>최신 영화</h2>
+        <hr />
+
+        {/* Movie Grid Cards */}
+
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'center',}}>
+        <button>불러오기</button>
+      </div>
+
     </div>
+
+
   )
 }
 
